925de2715310a43ff2652325af9c6614ad97111f
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-04-17 22:53:54 -0400

    meals page authed & pulling in initial meal data

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ var routes = {
 exports = module.exports = function(app) {
 	// Views
 	app.get("/", routes.views.index);
-	app.get("/meals", routes.views.meals);
+	app.get("/meals", middleware.requireUser, routes.views.meals);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
--- routes/views/meals.js
+++ routes/views/meals.js
@@ -1,12 +1,16 @@
 var keystone = require("keystone");
+var Meal = keystone.list("Meal");
 
-exports = module.exports = function(req, res) {
+exports = module.exports = async function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = "meals";
+
+	//get a list of meals
+	locals.meals = await Meal.model.find().exec();
 
 	// Render the view
 	view.render("meals");
